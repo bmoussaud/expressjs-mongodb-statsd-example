60519321719ad23253684daaea8c8de4b67eb079
Benoit Moussaud <benoit@moussaud.org>
2022-11-22 08:06:00 +0100

    refactor

--- create-manually-managed-secret.sh
+++ create-manually-managed-secret.sh
@@ -1,12 +1,3 @@
-kubectl delete secret mongodb-credentials-local -n dev-tap
-kubectl create secret generic mongodb-credentials-local \
-    --from-literal=username='sample-mongodb-account-beta'  \
-    --from-literal=password='mMmWOSsEMniWDo9Uha1hrUMWae4g3Zzk3fzO2DBoMRHbtfeBWxsxCrcN4xRBJcHIwhfHNhN4Oj8oACDbNpNgdQ=='  \
-    --from-literal=host='sample-mongodb-account-beta.mongo.cosmos.azure.com'  \
-    --from-literal=port='10255'  \
-    --from-literal=type='mongodb'  \
-    -n dev-tap
-
 kubectl delete secret mongodb-credentials-remote -n mongodb-instances-fr
 kubectl delete secret mongodb-credentials-manually-managed -n mongodb-instances-fr
 kubectl create secret generic mongodb-credentials-manually-managed \
